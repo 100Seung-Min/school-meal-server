d158be2759d8dda41f3617eeccd12ed7dd8bf513
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-10-27 22:08:39 +0900

    시작

---
+++ src/main/kotlin/com/example/schoolmealserver/SchoolMealServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.schoolmealserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchoolMealServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SchoolMealServerApplication>(*args)
+ }
---
+++ src/test/kotlin/com/example/schoolmealserver/SchoolMealServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.schoolmealserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchoolMealServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
