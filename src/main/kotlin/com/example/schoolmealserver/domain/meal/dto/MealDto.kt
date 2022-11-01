f2e1c1c5306fce5535b378d0d5c2499b04c2a07e
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-01 20:02:29 +0900

    Dto SerializedName설정

--- src/main/kotlin/com/example/schoolmealserver/domain/meal/dto/MealDto.kt
+++ src/main/kotlin/com/example/schoolmealserver/domain/meal/dto/MealDto.kt
@@ -1,15 +1,22 @@
 package com.example.schoolmealserver.domain.meal.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class MealDto(
+        @SerializedName("mealServiceDietInfo")
         val mealServiceDietInfo: List<MealServiceDietInfo?>?
 ) {
     data class MealServiceDietInfo(
+            @SerializedName("row")
             val row: List<DietRow>?
     ) {
         data class DietRow(
-                val DDISH_NM: String,
-                val MLSV_YMD: String,
-                val MMEAL_SC_NM: String,
+                @SerializedName("DDISH_NM")
+                val dishName: String,
+                @SerializedName("MLSV_YMD")
+                val mealDay: String,
+                @SerializedName("MMEAL_SC_NM")
+                val mealTime: String,
         )
     }
 }
