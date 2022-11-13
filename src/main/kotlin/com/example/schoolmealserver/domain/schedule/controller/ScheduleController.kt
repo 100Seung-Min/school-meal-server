d86a60dbe3aaa108efc6219132dfa9e39447ebdb
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-13 09:19:15 +0900

    SchoolController month와 id로 변경

--- src/main/kotlin/com/example/schoolmealserver/domain/schedule/controller/ScheduleController.kt
+++ src/main/kotlin/com/example/schoolmealserver/domain/schedule/controller/ScheduleController.kt
@@ -1,21 +1,24 @@
 package com.example.schoolmealserver.domain.schedule.controller
 
+import com.example.schoolmealserver.domain.auth.service.AuthService
 import com.example.schoolmealserver.domain.schedule.payload.response.ScheduleResponse
 import com.example.schoolmealserver.global.openapi.URLList
 import com.example.schoolmealserver.global.openapi.connectSchedule
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.net.URL
 
 @RestController
-class ScheduleController {
+class ScheduleController(
+        private val authService: AuthService
+) {
     @GetMapping("/schedule")
     fun schedule(
-            @RequestParam(name = "cityCode") cityCode: String,
-            @RequestParam(name = "schoolCode") schoolCode: String,
+            @RequestHeader("id") id: String,
             @RequestParam(name = "month") month: String
     ): ScheduleResponse? {
-        return connectSchedule(URL("${URLList.schedule}ATPT_OFCDC_SC_CODE=$cityCode&SD_SCHUL_CODE=$schoolCode&AA_YMD=$month"))
+        return connectSchedule(URL("${URLList.schedule}ATPT_OFCDC_SC_CODE=${authService.getUser(id).cityCode}&SD_SCHUL_CODE=${authService.getUser(id).schoolCode}&AA_YMD=$month"))
     }
 }
