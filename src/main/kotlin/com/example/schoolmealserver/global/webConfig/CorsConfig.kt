7d4f32a03d9de1c597dead3f848df0add6ee0572
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-16 09:51:32 +0900

    WebConfig CORS설정

---
+++ src/main/kotlin/com/example/schoolmealserver/global/webConfig/CorsConfig.kt
@@ -0,0 +1,17 @@
+package com.example.schoolmealserver.global.webConfig
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig: WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .allowedMethods("GET")
+                .maxAge(3000)
+    }
+}
